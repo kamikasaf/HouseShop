71e3147db22ac3c542d4e8cf2f7917fe2eec7fe7
Erlashka <slayer6662mail.ru@gmail.com>
2022-06-14 15:30:47 +0600

    startproject

---
+++ apps/account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from apps.account.views import *
+
+urlpatterns = [
+    path('register/', RegistrationView.as_view()),
+    path('activate/<str:activation_code>/', ActivationView.as_view()),
+    path('login/', LoginAPIView.as_view()),
+    # path('logout/', LogoutAPIView.as_view()),
+    path("forgot_password/", ForgotPasswordView.as_view()),
+    path('confirm_email/<str:activation_code>/', TakeNewPasswordView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view())
+]
