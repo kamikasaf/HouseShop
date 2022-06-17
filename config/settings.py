17e6320c6c9ba9a1dc1659268cbecea3433fd81e
Aigerim Begimbaeva <aigerimbegimbaeva@MacBook-Air-Aigerim.local>
2022-06-17 15:19:32 +0600

    database change

--- config/settings.py
+++ config/settings.py
@@ -100,17 +100,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT',cast=int),
+        'ENGINE': config('DB_ENGINE')
+    #     'NAME': config('DB_NAME'),
+    #     'USER': config('DB_USER'),
+    #     'PASSWORD': config('DB_PASSWORD'),
+    #     'HOST': config('DB_HOST'),
+    #     'PORT': config('DB_PORT',cast=int),
     }
 }
 
-# import dj_database_url
-# DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
+import dj_database_url
+DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 
 # Password validation
