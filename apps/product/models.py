b202abced16c96bb5113efc5f94e434e4c719ea1
Aigerim Begimbaeva <aigerimbegimbaeva@MacBook-Air-Aigerim.local>
2022-06-22 21:59:38 +0600

    img

--- apps/product/models.py
+++ apps/product/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     description = models.TextField() 
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
-    img = models.ImageField(upload_to='products/')
+    img = models.ImageField(upload_to='products/', blank=True, null=True)
     available = models.BooleanField(default=True)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
